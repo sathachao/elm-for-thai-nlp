889d538244f557b8905cf46b2a05c9a32339b61b
sathachao <sathachao@gmail.com>
2017-04-20 14:45:53 +0700

    [Neural Net] Fixed ELM's Test Method

--- LearningProcess/ELMModel.py
+++ LearningProcess/ELMModel.py
@@ -16,31 +16,22 @@ class ELMModel:
 
     def test(self, xTest, yTest):
         yPredicted = self.model.predict(xTest)
-        print(confusion_matrix(y_true=np.argmax(xTest, axis=1), y_pred=np.argmax(yTest, axis=1)))
-
+        print(confusion_matrix(y_true=np.argmax(yTest, axis=1), y_pred=np.argmax(yPredicted, axis=1)))
 
 ########Example for ELM
-# iris = datasets.load_iris()
-# 
-# X = iris.data
-# Y = np.zeros((len(iris.target), 3), dtype=np.int)
-# 
-# temp = iris.target
-# 
-# for i in range(len(temp)):
-#     Y[i][temp[i]] = 1
-# 
-# 
-# x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
-# 
-# elmModel = hpelm.ELM(inputs=4, outputs=3, classification="c")
-# 
-# elmModel.add_neurons(number=20, func="sigm")
-# elmModel.train(X=x_train, T=y_train)
-# 
-# y_predicted = elmModel.predict(x_test)
-# 
-# # y_predicted = y_predicted.argmax[1]
-# # y_test = y_test.argmax[1]
-# 
-# print(confusion_matrix(y_true=np.argmax(y_test, axis=1), y_pred=np.argmax(y_predicted, axis=1)))
+iris = datasets.load_iris()
+
+X = iris.data
+Y = np.zeros((len(iris.target), 3), dtype=np.int)
+
+temp = iris.target
+
+for i in range(len(temp)):
+    Y[i][temp[i]] = 1
+
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+elmModel = ELMModel(4, 3, 1, 20)
+elmModel.train(x_train, y_train)
+elmModel.test(x_test, y_test)
